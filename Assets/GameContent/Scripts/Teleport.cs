e2a1ca6a0f3d9792429174af6ee489cd43e297c5
Skylar Hu <alphabet_he@outlook.com>
2023-09-26 11:29:49 -0600

    Update teleport

--- Assets/GameContent/Scripts/Teleport.cs
+++ Assets/GameContent/Scripts/Teleport.cs
@@ -5,20 +5,30 @@ using UnityEngine;
 public class Teleport : MonoBehaviour
 {
     public Vector3 RoomOneTeleport;
+    public Vector3 RoomOneRotation;
+
     public Vector3 RoomTwoTeleport;
+    public Vector3 RoomTwoRotation;
 
     void MovePosition(Vector3 position)
     {
         gameObject.transform.position = position;
     }
 
+    public void MoveRotation(Vector3 rotation) 
+    {
+        transform.rotation = Quaternion.Euler(rotation);
+    }
+
     public void ToRoomOne()
     {
         MovePosition(RoomOneTeleport);
+        MoveRotation(RoomOneRotation);
     }
 
     public void ToRoomTwo()
     {
         MovePosition(RoomTwoTeleport);
+        MoveRotation(RoomTwoRotation);
     }
 }
